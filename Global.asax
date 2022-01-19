6271c992bbec8597734604c7b9aaaa9cf8b5708b
SoumikMorial <97872252+SoumikMorial@users.noreply.github.com>
2022-01-19 20:40:18 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assessment4._2.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assessment4._2.Startup))]
+namespace Assessment4._2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
